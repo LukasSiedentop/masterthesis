7bff1336a7706bf04aa1bec044698c7c1ee76564
Lukas Siedentop <lukas.siedentop@uni.kn>
2015-07-15 16:13:26 +0200

    NearestNeighbour works, result can be found in the eclipse console.

--- org.knime.knip.pointextraction/src/org/knime/knip/pointextraction/GetNearestNeighbours.java
+++ org.knime.knip.pointextraction/src/org/knime/knip/pointextraction/GetNearestNeighbours.java
@@ -7,7 +7,6 @@ import net.imagej.ImgPlus;
 import net.imglib2.Cursor;
 import net.imglib2.RandomAccess;
 import net.imglib2.labeling.Labeling;
-import net.imglib2.labeling.LabelingType;
 import net.imglib2.roi.RectangleRegionOfInterest;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
@@ -39,14 +38,16 @@ public class GetNearestNeighbours<BitType extends RealType<BitType>> implements 
 
                 RandomAccess<BitType> inRndAccess = input.randomAccess();
 
-                long[] posi = new long[labeling.numDimensions()];
+                int[] nodePixelPosition = new int[labeling.numDimensions()];
+                int[] nextOnLine = new int[labeling.numDimensions()];
+
                 Integer node = 0;
 
                 // ROI generieren mit Fenstergröße um den Ursprung 0^d
                 double[] extend = new double[labeling.numDimensions()];
                 Arrays.fill(extend, 3);
                 double[] displacement = new double[labeling.numDimensions()];
-                Arrays.fill(displacement, -3.0 / 2.0);
+                Arrays.fill(displacement, -1);
                 final RectangleRegionOfInterest roi = new RectangleRegionOfInterest(new double[labeling.numDimensions()], extend);
 
                 // Iteration über alle Knoten
@@ -61,18 +62,40 @@ public class GetNearestNeighbours<BitType extends RealType<BitType>> implements 
                                 nodeCur.fwd();
 
                                 // Inputposition auf labelpixel setzen
-                                nodeCur.localize(posi);
-                                inRndAccess.setPosition(posi);
+                                nodeCur.localize(nodePixelPosition);
+                                inRndAccess.setPosition(nodePixelPosition);
 
-                                System.out.println("Labelpixel: " + nodeCur.get());
-                                System.out.println("Inputpixel: " + inRndAccess.get());
-                                System.out.println("Position: " + Arrays.toString(posi));
-                                System.out.println("Area: " + labeling.getArea(node));
+                                // ROI um nodepixel legen
+                                roi.setOrigin(nodeCur);
+                                roi.move(displacement);
 
-                                RandomAccess<LabelingType<Integer>> labelingCur = labeling.randomAccess(input);
-                                labelingCur.setPosition(posi);
+                                Cursor<BitType> roiCursor = roi.getIterableIntervalOverROI(input).cursor();
 
-                                System.out.println("LabelingCur: " + labelingCur.get());
+                                // über ROI um Knotenpixel iterieren
+                                while (roiCursor.hasNext()) {
+                                        roiCursor.fwd();
+
+                                        if (roiCursor.get().getRealDouble() != 0) {
+                                                roiCursor.localize(nextOnLine);
+
+                                                if (!labeling.getRegionOfInterest(node).contains(copyFromIntArray(nextOnLine))) {
+
+                                                        Integer nextNode = walk(nextOnLine, nodePixelPosition);
+                                                        System.out.println("Nachbar von " + node + " ist " + nextNode);
+                                                }
+                                        }
+                                }
+                                /*
+                                                                System.out.println("Labelpixel: " + nodeCur.get());
+                                                                System.out.println("Inputpixel: " + inRndAccess.get());
+                                                                System.out.println("Position: " + Arrays.toString(posi));
+                                                                System.out.println("Area: " + labeling.getArea(node));
+
+                                                                RandomAccess<LabelingType<Integer>> labelingCur = labeling.randomAccess(input);
+                                                                labelingCur.setPosition(posi);
+
+                                                                System.out.println("LabelingCur: " + labelingCur.get());
+                                                                */
                         }
 
                 }
@@ -96,5 +119,64 @@ public class GetNearestNeighbours<BitType extends RealType<BitType>> implements 
                 BufferedDataTable table = buf.getTable();
                  */
 
+        }
+
+        private Integer walk(int[] current, int[] last) {
+
+                // den nächsten Schritt ermitteln
+                int[] next = new int[labeling.numDimensions()];
+
+                // ROI generieren mit Fenstergröße um den Ursprung 0^d
+                double[] extend = new double[labeling.numDimensions()];
+                Arrays.fill(extend, 3);
+                double[] displacement = new double[labeling.numDimensions()];
+                Arrays.fill(displacement, -1);
+                final RectangleRegionOfInterest roi = new RectangleRegionOfInterest(new double[labeling.numDimensions()], extend);
+                // ROI um current legen
+                roi.setOrigin(copyFromIntArray(current));
+                roi.move(displacement);
+
+                Cursor<BitType> roiCursor = roi.getIterableIntervalOverROI(input).cursor();
+
+                // über ROI iterieren
+                while (roiCursor.hasNext()) {
+                        roiCursor.fwd();
+
+                        if (roiCursor.get().getRealDouble() != 0) {
+                                roiCursor.localize(next);
+
+                                // Abbruchbedingungen checken
+                                // Abbruchbedingung: wenn der nächste Schritt nicht zurückgeht...
+                                if (!Arrays.equals(next, last) && !Arrays.equals(next, current)) {
+                                        Iterator<Integer> nodesIter = labeling.getLabels().iterator();
+                                        Integer node = 0;
+                                        // über alle Knoten iterieren, und wenn next gelabelt ist diesen Knoten zurückgeben
+                                        while (nodesIter.hasNext()) {
+                                                node = nodesIter.next();
+                                                if (labeling.getRegionOfInterest(node).contains(copyFromIntArray(next))) {
+                                                        return node;
+                                                }
+                                        }
+
+                                        // wenn ein weißer Pixel gefunden ist, der weiß ist und nicht gelabelt, weitergehen
+                                        return walk(next, current);
+                                }
+
+                        }
+                }
+
+                // Abbruchbedingung: kein Pixel um current außer last ist mehr weiß: kein Nachbar
+                return -99;
+
+        }
+
+        // Hack: convert to int[] double[], damit roi.contains funktioniert. Diskussion:
+        // http://stackoverflow.com/questions/12729139/copy-contents-of-an-int-array-to-a-double-array-in-java
+        private static double[] copyFromIntArray(int[] source) {
+                double[] dest = new double[source.length];
+                for (int i = 0; i < source.length; i++) {
+                        dest[i] = source[i];
+                }
+                return dest;
         }
 }
