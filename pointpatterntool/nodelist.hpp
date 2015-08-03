8c44c62822241421bedf5ec48c67ff453b159266
Lukas Siedentop <lukas@siedentop.de>
2015-08-03 09:34:48 +0200

    minor changes

--- pointpatterntool/nodelist.hpp
+++ pointpatterntool/nodelist.hpp
@@ -16,18 +16,19 @@
 #include <iostream>
 #include <cmath>
 
-
 /**
  * Kopf der Liste. Hier stecken Daten wie Listenlänge, erstes Element, letztes Element drin, die sonst jedesmal in jedem Knoten der Liste geändert werden müssten.
  */
 class NodeHead {
 private:
 	/* Listenspezifisches */
-	class Node * first, *last;
+	// Zeiger auf das erste und letzte Element
+	class Node *first, *last;
+	// Länge der Liste, mit 0 initialisiert
 	int size;
 
 	/* Datenspezifisches */
-	//... ?
+	double minX, minY, minZ, maxX, maxY, maxZ;
 public:
 	NodeHead();
 	~NodeHead();
@@ -39,10 +40,14 @@ public:
 	void add(Node * node);
 	// löscht das Element node
 	void remove(Node * node);
-	//...
+	// zeigt die Liste an.
+	void display();
+	// den ersten Knoten geben
+	Node * getFirst();
 
 	/* Datenspezifisches */
-	//... ?
+	// überprüft ob ein Knoten an einem Punkt exisitiert
+	Node * getAt(double x, double y, double z);
 };
 
 /**
@@ -51,14 +56,20 @@ public:
 class Node {
 private:
 	/* Listenspezifisches */
-	class NodeHead * head;
-	Node * prev, *next;
-
+	// Zeiger auf den Listenkopf
+	class NodeHead *head;
+	// Zeiger auf Vorgänger und Nachfolger, mit NULL initialisiert
+	Node *prev, *next;
 
 	/* Datenspezifisches */
-	// Position
+	// Position TODO: als array oder wie bei Dirk als extra Klasse
 	double x, y, z;
-	// Verbundene Nachbarn, Distanz zu Nachbarn, Winkel zu Nachbarn, Voronoi Nachbarn,...
+	// Verbundene Nachbarn
+	class Neighbour *neighbours;
+	//Distanz zu Nachbarn, Winkel zu Nachbarn, Voronoi Nachbarn,...
+
+	// prüft ob der gegebene Knoten schon ein Nachbar ist
+	bool isNeighbour(Node * node);
 public:
 	Node();
 	Node(double x, double y, double z);
@@ -67,7 +78,7 @@ public:
 	/* Listenspezifisches */
 	// vertauscht diesen Knoten mit node.
 	void swap(Node * node);
-	// getter und setter (warum nochmal? brauchen wir wirklich private und so?)
+	// getter und setter
 	void setHead(NodeHead * newHead);
 	void setNext(Node * node);
 	void setPrev(Node * node);
@@ -76,12 +87,45 @@ public:
 	Node * getNext();
 	Node * getPrev();
 	double getX();
-	//...
+	double getY();
+	double getZ();
+	Neighbour* getNeighbours();
 
 	/* Datenspezifisches */
 	// Gibt die Euklidsche Distanz zu node zurück.
 	double euklidian(Node * node);
+	// berechnet den Winkel zwischen diesem und den zwei gegebenen Knoten
+	double angle(Node * nodeA, Node * nodeB);
+	// fügt der Nachbarnliste einen weiteren Nachbarn hinzu
+	void addNeighbour(Node * node);
+	// vergleicht diesen Knoten mit dem gegebenen, wenn beide an der gleichen Position sind, sind sie gleich (TODO: Nachbarn und so auch vergleichen?)
+	bool equals(Node * node);
+	// zählt die Nachbarn
+	int countNeighbours();
+
 };
 
+/**
+ * Nachbarn eines Knotens in Form einer einfach verketteten Liste.
+ */
+class Neighbour {
+private:
+	Neighbour *next;
+	Node * node;
+public:
+	Neighbour();
+	Neighbour(Node * node);
+	~Neighbour();
+
+	// setter & getter
+	Node * getNode();
+	Neighbour * getNextNeighbour();
+
+	void add(Node * node);
+};
+
+/**
+ * Position?
+ */
 
 #endif /* NODELIST_HPP_ */
